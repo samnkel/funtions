d72e8d005921f545aa8a3e929f7993e18ff9b82f
samnkel <140614525+samnkel@users.noreply.github.com>
2025-11-03 14:04:32 +0200

    Add files via upload
    completed exercise

---
+++ funtions.js
@@ -0,0 +1,19 @@
+
+function greet(name) {
+
+  return "Hello, " + name ;
+}
+
+
+function add(num1, num2) {
+
+  return num1 + num2;
+}
+
+
+let greetingResult = greet("SAM");
+let sumResult = add(9, 7);
+
+
+console.log(greetingResult); 
+console.log(sumResult);     
