4268d56f13f679e3b4facdda4666c4ab5598693d
samnkel <140614525+samnkel@users.noreply.github.com>
2025-11-03 13:55:40 +0200

    Add files via upload
    completed

---
+++ condition.js
@@ -0,0 +1,13 @@
+let score = 80;
+let passGrade = 60;
+
+if (score === 100){
+    console.log ("perfect");
+}else if (score >= passGrade){
+     console.log("pass");
+}else{ 
+    console.log("fail")
+}
+
+
+
